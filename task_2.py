790554ecd64937167c1f557d2d1aa5f26e2a3e2f
zhukovso <s.zhukov.2010@gmail.com>
2024-04-21 23:42:00 +0300

    Second task with random.sample

--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import sample
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     def validate_input_numbers(number: int) -> int:
@@ -7,23 +7,25 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
         else:
             raise ValueError(number)
 
-    result_set = set()
+    # result_set = set()
     try:
-        min = validate_input_numbers(min)
-        max = validate_input_numbers(max)
-        quantity = validate_input_numbers(quantity)
+        # min = validate_input_numbers(min)
+        # max = validate_input_numbers(max)
+        # quantity = validate_input_numbers(quantity)
 
-        while len(result_set) < quantity:
-            result_set.add(randint(min, max))
+        # while len(result_set) < quantity:
+        #     result_set.add(randint(min, max))
+
+        return sorted(sample(range(validate_input_numbers(min), validate_input_numbers(max)), validate_input_numbers(quantity)))
 
     except ValueError as e:
         print(f"min, max or quantity is not a valid number. Expected number beetwen 1 and 1000!")
     except TypeError as e:
         print(f"min, max or quantity is not a number. Please input a number beetwen 1 and 1000!")
 
-    return  sorted(list(result_set))
+    # return  sorted(list(result_set))
 
-# print(get_numbers_ticket(1, 1000, 10))
-# print(get_numbers_ticket(0, 1000, 10))
-# print(get_numbers_ticket("1", 1000, 10))
-# print(get_numbers_ticket(10, None, 100))
+print(get_numbers_ticket(1, 1000, 10))
+print(get_numbers_ticket(0, 1000, 10))
+print(get_numbers_ticket("1", 1000, 10))
+print(get_numbers_ticket(10, None, 100))
